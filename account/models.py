ec68765a7102a9af9cf82a0c3fe3acb989e10ae4
Sam <samsuchin@gmail.com>
2021-09-11 18:05:17 -0400

    made facial recognition for works

--- account/models.py
+++ account/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 from .managers import CustomUserManager
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
--- static/js/detect.js
+++ static/js/detect.js
@@ -1,19 +1,42 @@
 
 const video = document.getElementById("video");
 const canvas = document.getElementById("overlay");
-const expressionEl = document.getElementById("expressions")
-Promise.all([
-    faceapi.loadFaceExpressionModel("/static/models"),
-    faceapi.loadSsdMobilenetv1Model("/static/models"),
-]).then(startVideo)
+const expressionEl = document.getElementById("expressions");
+const video_view = document.getElementById("video-view");
+const video_icon = document.getElementById("video-icon")
+
+async function startLaughAnalysis(){
+    Promise.all([
+        faceapi.loadFaceExpressionModel("/static/models"),
+        faceapi.loadSsdMobilenetv1Model("/static/models"),
+    ]).then(startVideo)
+}
+
+function stopVideo(){
+    var stream = video.srcObject;
+    var tracks = stream.getTracks();
+    for (var i = 0; i < tracks.length; i++) {
+        var track = tracks[i];
+        track.stop();
+    }
+    
+    video.srcObject = null;
+    canvas.style.display = "none"
+    document.getElementById("video-stop").style.display = "none";
+    document.getElementById("video-start").style.display = "block";
+}
 
 function startVideo(){
-
+    alert("Start reading the article with a straight face and only one face in the camera view.")
     if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
       video.srcObject = stream;
       video.play()
+      video_view.style.display = "block"
+      canvas.style.display = "block"
+        document.getElementById("video-stop").style.display = "block";
+        document.getElementById("video-start").style.display = "none";
       refreshState()
     })
     .catch(function (err) {
@@ -27,14 +50,13 @@ async function refreshState() {
     setInterval(async() => {
         try{
             const detections = await faceapi.detectSingleFace(video).withFaceExpressions()
-            console.log(detections.expressions)
             if (detections){
-                console.log(detections.expressions)
                 let happiness = Math.trunc(detections.expressions.happy * 100)
-                let disgusted = Math.trunc(detections.expressions.disgusted * 100)
-                document.getElementById("happiness").innerHTML = happiness
-                document.getElementById("disgusted").innerHTML = disgusted
                 drawResults(detections)
+                if(happiness > 98){
+                    confetti()
+                    console.log("CONFETTI")
+                }
             }
         }
         catch(error){
@@ -46,8 +68,37 @@ async function refreshState() {
 }
 
 function drawResults(detections){
+    let happiness = Math.trunc(detections.expressions.happy * 100)
       faceapi.matchDimensions(canvas, video)
       const resizedResults = faceapi.resizeResults(detections, video)
       const minConfidence = 0.05
-      faceapi.draw.drawDetections(canvas, resizedResults)
+      faceapi.draw.drawDetections(canvas, resizedResults)   
+      var ctx = canvas.getContext("2d");
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, canvas.height - 30, canvas.width, 30);
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.fillText("Happiness: " + happiness + "%", canvas.width/2, canvas.height - 10); 
 }
+
+function random(max){
+    return Math.random() * (max - 0) + 0;
+}
+
+function confetti(){
+    var c = document.createDocumentFragment();
+    for (var i=0; i<100; i++) {
+        var styles = 'transform: translate3d(' + (random(500) - 250) + 'px, ' + (random(200) - 150) + 'px, 0) rotate(' + random(360) + 'deg);\
+                      background: hsla('+random(360)+',100%,50%,1);\
+                      animation: bang 1000ms ease-out forwards;\
+                      opacity: 0';
+          
+        var e = document.createElement("i");
+        e.classList.add("conf")
+        e.style.cssText = styles.toString();
+        c.appendChild(e);
+        video_view.append(c)
+    }
+}
+
