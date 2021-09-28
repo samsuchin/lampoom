1a43a5b157ff7dc76c5faeab253c541f525fdc54
Sam <samsuchin@gmail.com>
2021-09-28 15:05:33 -0400

    fixed user

--- account/models.py
+++ account/models.py
@@ -22,7 +22,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     board = models.CharField(choices=ACCOUNT.BOARD_CHOICES, max_length=20)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'display_name', 'profile_url_name', 'url_username']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'display_name', 'url_username']
 
     objects = CustomUserManager()
 
