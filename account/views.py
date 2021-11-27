f2c8642eb79e635b52e89656a7f655e1b8de17f7
Sam Suchin <samsuchin@Sams-MacBook-Pro.local>
2021-11-26 22:50:26 -0500

    fixed index feature and works

--- account/views.py
+++ account/views.py
@@ -10,7 +10,7 @@ from .forms import UserSettingsForm
 
 def account_detail(request, url_username):
     account = get_object_or_404(get_user_model(), url_username=url_username)
-    works = Work.objects.filter(active=True).filter(Q(writer=account) | Q(art_work__artist=account)).filter(original_work=True, art_work__original_work=True).distinct().order_by('-created_at')
+    works = Work.objects.filter(active=True).filter(Q(writer=account) | Q(art_work__artist=account)).filter(original_work=True).distinct().order_by('-created_at')
     context = {
         "account": account,
         "works": works
